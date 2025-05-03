917eda36e7ac702501d264f23d93b7d3472ac101
Ladzuu <lucaslabiouse1243@gmail.com>
2025-05-03 12:10:26 +0200

    NavBar Mobile + Anim

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,3 +3,59 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+// BURGER MENU
+
+const menu = document.querySelector(".nav__mb");
+const menuButton = document.querySelector(".menu--btn");
+
+menuButton.addEventListener("click", function () {
+    const menuLinks = document.querySelectorAll(".nav__list .nav__link");
+
+    // Ajout de la classe "open" pour le bouton
+    menuButton.classList.toggle("open");
+
+    if (menu.classList.contains("open")) {
+        // Fermer le menu
+        gsap.to(menuLinks, {
+            opacity: 0,
+            duration: 0.2,
+            stagger: { each: 0.1, from: "end" }, // Animation du dernier au premier
+            onComplete: function () {
+                // Une fois les liens disparus, fermeture du menu
+                gsap.to(menu, {
+                    y: "-100%",
+                    duration: 0.5,
+                    ease: "power4.in",
+                });
+                menu.classList.remove("open");
+            },
+        });
+    } else {
+        // Ouvrir le menu
+        gsap.fromTo(
+            menu,
+            { y: "-100%" },
+            {
+                y: "0%",
+                duration: 0.5,
+                ease: "power4.out", // Easing pour une ouverture lente au début
+                onComplete: function () {
+                    // Animation des liens
+                    gsap.fromTo(
+                        menuLinks,
+                        { opacity: 0 },
+                        {
+                            opacity: 1,
+                            duration: 0.5,
+                            stagger: 0.2,
+                            ease: "power2.out",
+                        }
+                    );
+                },
+            }
+        );
+
+        menu.classList.add("open");
+    }
+});
